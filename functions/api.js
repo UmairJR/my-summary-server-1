599912208568321c558f2e614b7eb4477bd7b521
Umair Shaikh <umairjr1265@gmail.com>
2024-08-23 18:49:35 +0530

    changing domain

--- functions/api.js
+++ functions/api.js
@@ -35,7 +35,7 @@ router.get("/", (req, res) => {
 });
 
 router.get('/api/upload', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://vidsum-ai.vercel.app/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://vidsum-ai.vercel.app');
 
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
 
@@ -49,7 +49,7 @@ router.get('/api/upload', (req, res) => {
 
 
 router.post('/api/upload', async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://vidsum-ai.vercel.app/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://vidsum-ai.vercel.app');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   const chunks = [];
